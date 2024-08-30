7756e4697688cf00b877522ca975e20cf41a641f
guscao <113106462+guscao@users.noreply.github.com>
2024-08-30 10:33:51 -0400

    30

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ const perguntas = [
             "a) A falta de regulamentação no mercado financeiro.",
             "b) A alta carga tributária sobre as grandes fortunas."
         ]
-    }
+    },
     {
         enunciado: "Qual é uma das principais consequências da falta de acesso à educação de qualidade para crianças em áreas carentes?",
         alternativas: [
             "a) Maior taxa de analfabetismo e menor oportunidades de emprego no futuro.",
             "b) Aumento na qualidade das infraestruturas públicas."
         ]
-    }
+    },
 ];
 
 let atual = 0;
