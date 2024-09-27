be88de8dbf41acdbaa33cc1a8e64ae07eca08ac4
guscao <113106462+guscao@users.noreply.github.com>
2024-09-27 10:04:03 -0400

    pepapig

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFInal = "";
 
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
@@ -46,15 +47,20 @@ function mostraPergunta() {
 }
 
 function mostraAlternativas() {
-    for (const alternativa of perguntaAtual.alternativas) {
+    for(const alternativa of perguntaAtual.alternativas) {
       const botaoAlternativas = document.createElement("button");
       botaoAlternativas.textContent = alternativa.texto;
-      botaoAlternativas.addEventListener("click", function () {
-        atual++;
-        mostraPergunta();
-      });
+      botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
       caixaAlternativas.appendChild(botaoAlternativas);
     }
-  }
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    const afircoes = opcaoSelecionada.afircoes;
+    historiaFInal = afircoes;
+    atual++;
+    mostraPergunta();
+ }
+     
 mostraPergunta()
 
